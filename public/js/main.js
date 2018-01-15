8b8328c9bca464c74bcb66701e65a1f8c087e38d
Charles Davis <charles.davis@borngroup.com>
2018-01-15 15:43:53 -0500

    part 2 of mario complete:

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,42 @@
-import SpriteSheet from './spriteSheets.js';
-import {loadImage, loadLevel} from './loaders.js';
-
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-            for (let y = y1; y < y2; ++y) {
-                sprites.drawTile(background.tile, context, x , y);
-            }
-        }
-    });
-}
+import Compositor from './Compositor.js';
+import {loadLevel} from './loaders.js';
+import {loadMarioSprites, loadBackgroundSprites} from './sprites.js';
+import {createBackgroundLayer} from './layers.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
+function createSpriteLayer(sprite, pos) {
+    return function drawSpriteLayer(context) {
+        sprite.draw('idle', context, pos.x, pos.y);
+    };
+}
 
-loadImage('/img/tiles.png')
-.then(image => {
-    const sprites = new SpriteSheet(image, 16, 16);
-    sprites.define('ground', 0, 0);
-    sprites.define('sky', 3, 23);
+Promise.all([
+    loadMarioSprites(),
+    loadBackgroundSprites(),
+    loadLevel('1-1'),
+])
+.then(([marioSprite, sprites, level]) => {
+    const comp = new Compositor();
 
-    loadLevel('1-1')
-    .then(level => {
-        level.backgrounds.forEach(background => {
-            drawBackground(background, context, sprites);
-        });
-    })
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites);
+    comp.layers.push(backgroundLayer);
 
+    const pos = {
+        x: 64,
+        y: 64
+    };
+
+    const spriteLayer = createSpriteLayer(marioSprite, pos);
+    comp.layers.push(spriteLayer);
     
-})
+    function update() {
+        comp.draw(context);
+        pos.x += 2;
+        pos.y += 2;
+        requestAnimationFrame(update);
+    }
+
+    update();
+});
